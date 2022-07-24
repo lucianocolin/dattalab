7131383c41852c7724516b9dcd3f99ff8d1e7487
lucianocolin <lucianocolin99@gmail.com>
2022-07-24 15:22:05 -0300

    Demo Home designed

--- dattalab-react/src/pages/Home.jsx
+++ dattalab-react/src/pages/Home.jsx
@@ -7,12 +7,24 @@ import '../css/Pages/Home.css';
 const Home = () => {
   return (
     <main>
+      <div className='main-title'>
+        <h1>Consultora especializada en comunicación política</h1>
+        <h3>Asesoría. Encuestas en vivo. Consultoría en general</h3>
+      </div>
       <div className='slider'>
         <ul>
           <li><img src={Slider1} alt="Slider 1" height='700px' width='100%' /></li>
           <li><img src={Slider2} alt="Slider 2" height='700px' width='100%' /></li>
           <li><img src={Slider3} alt="Slider 3" height='700px' width='100%' /></li>
         </ul>
+      </div>
+      <div className='us-section'>
+        <h2>Dattalab Consultora</h2>
+        <p>Somos una consultora especializada en comunicación política con 10 años de trayectoría. 
+          Nos dedicamos a la asesoría personalizada mediante consultoría privada, encuestas con un sistema 
+          propio y bases de datos actualizadas para obtener resultados reales, orientando a nuestros clientes
+          sobre su imágen pública, la de su equipo o partido político y sobre el pensamiento general sobre un 
+          tema en específico.  </p>
       </div>
     </main>
   )
